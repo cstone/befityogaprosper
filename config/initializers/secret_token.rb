592507387192bccb7bfc48dff908ca7e6cd52b85
Cavin <cstone2003@hotmail.com>
2014-06-24 11:50:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Befityogaprosper::Application.config.secret_token = 'c644bf0e80238702a32ca2efa5ceba63ad39fd342be958350a8daa28da6b2b1e33a1407263475bd69d3a069d290d3f4ce46df711c348de78039daf46f4115cf4'
