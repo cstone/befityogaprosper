a736a045ded07834b3f87a3056bd38d6d186a660
Cavin <cstone2003@hotmail.com>
2014-07-08 23:09:39 -0400

    Add :remove_page_image and :page_image_cache

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,9 @@
 class Page < ActiveRecord::Base
-  attr_accessible :content, :page_image, :permalink, :title
+  attr_accessible :content, :page_image, :permalink, :title, :remove_page_image, :page_image_cache
 
   mount_uploader :page_image, PageImageUploader
+
+  def to_param
+    permalink
+  end
 end
