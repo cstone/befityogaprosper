fa82743ef3f0b1852217c35a491ca7ba9281d187
Cavin <cstone2003@hotmail.com>
2014-09-09 11:55:38 -0400

    Update attributes for mass assignment

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,5 +1,5 @@
 class Staff < ActiveRecord::Base
-  attr_accessible :active, :bio, :name, :photo
+  attr_accessible :active, :bio, :first_name, :last_name, :title, :order, :remove_photo, :photo, :photo_cache
 
   mount_uploader :photo, StaffPhotoUploader
 
