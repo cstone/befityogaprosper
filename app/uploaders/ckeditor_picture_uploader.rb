2511791d0d9f6bad2a64f01c4d23e3db35105f4c
Cavin <cstone2003@hotmail.com>
2014-09-15 19:44:45 -0400

    Update ckeditor uploader for Amazon S3

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -1,19 +1,30 @@
 # encoding: utf-8
 class CkeditorPictureUploader < CarrierWave::Uploader::Base
-  include Ckeditor::Backend::CarrierWave
 
-  # Include RMagick or ImageScience support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-  # include CarrierWave::ImageScience
+  # Include RMagick or MiniMagick support:
+  include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
+  # storage :fog
+  storage :fog
+
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/ckeditor/pictures/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -28,7 +39,11 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  process :read_dimensions
+  # process :read_dimensions
+
+  process :resize_to_limit => [750, 0]
+
+
 
   # Create different versions of your uploaded files:
   version :thumb do
@@ -36,7 +51,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   end
 
   version :content do
-    process :resize_to_limit => [800, 800]
+    process :resize_to_limit => [750, 0]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
