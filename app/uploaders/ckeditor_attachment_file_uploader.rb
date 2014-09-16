213acee6cfb3706eb67ddac6c6e4130743ffb431
Cavin <cstone2003@hotmail.com>
2014-09-15 21:32:08 -0400

    Update attachment uploader.

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -2,18 +2,30 @@
 class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
 
-  # Include RMagick or ImageScience support:
-  # include CarrierWave::RMagick
+  # Include RMagick or MiniMagick support:
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
+  # storage :fog
+  storage :fog
+
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/ckeditor/attachments/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
