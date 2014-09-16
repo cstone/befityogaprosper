ac52f71ad44992cda771a6edd1d3b9fa4f7a91b7
Cavin <cstone2003@hotmail.com>
2014-09-15 21:07:54 -0400

    Change config.fog_public to true

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,6 +11,6 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"], # required
   }
   config.fog_directory  = ENV["AWS_S3_BUCKET"]                     # required
-  config.fog_public     = false                                 # optional, defaults to true
+  config.fog_public     = true                                 # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
